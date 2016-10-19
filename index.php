6d96d8c6e7da47573e51e6239302ffbb307a026b
avi-aminov <aviaminov.co.il@gmail.com>
2016-10-19 22:08:32 +0200

    WP Plugin Subscription Popup

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php 
+    /*
+    Plugin Name: WP Plugin Subscription Popup
+    Plugin URI: https://github.com/avi-aminov
+    Description: Subscription Popup Simple Plugin
+    Author: Avi Aminov
+    Version: 1.0
+    Author URI: https://github.com/avi-aminov
+    */
+?>
