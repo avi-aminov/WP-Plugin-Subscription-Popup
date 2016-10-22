aeb84fcf7fcf4e29f042d9e946107b165af2a54f
avi-aminov <aviaminov.co.il@gmail.com>
2016-10-22 22:00:34 +0200

    localstorage + X image

--- includes/shortcode.php
+++ includes/shortcode.php
@@ -1,41 +1,37 @@
 <?php 
 
     function subscriptionPopupFunc($atts) {
-        
-
         ob_start(); 
         $param = shortcode_atts( 
             array(
                 'displaytime' =>'1',
                 'title' => 'Subscribe to Our Newsletter',
-                'content' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-                'successMessage' => 'You are now subscribed to our newsletter',
-                'buttonText' => 'Subscribe',
+                'content' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry',
+                'successmessage' => 'You are now subscribed to our newsletter',
+                'buttontext' => 'Subscribe',
                 'placeholder' => 'Your email',
-                'sendEmail' => 'example@example.com',
+                'sendemail' => 'example@example.com',
             ), 
-        $atts, '');
-        
+        $atts, '');    
         ?>
-
         <div 
              id="subscription-popup-wrap" 
              align="center" 
-             data-success="<?php echo $param['successMessage']; ?>" 
-             data-email="<?php echo $param['sendEmail']; ?>" 
+             data-success="<?php echo $param['successmessage']; ?>" 
+             data-email="<?php echo $param['sendemail']; ?>" 
              data-time="<?php echo $param['displaytime']; ?>"
         >
-            <p id="subscription-popup-close">X</p>
+            <p id="subscription-popup-close"><img src="<?php  echo plugins_url( 'WP-Plugin-Subscription-Popup/assets/img/x.png'  ) ?>"/></p>
             <h1><?php echo $param['title']; ?></h1>
             <h2><?php echo $param['content']; ?></h2>
             <br/>
-            <input id="subscription-popup-value" placeholder="<?php echo $param['placeholder']; ?>"><button id="subscription-popup-send"><?php echo $param['buttonText']; ?></button>
+            <input id="subscription-popup-value" placeholder="<?php echo $param['placeholder']; ?>">
+            <button id="subscription-popup-send"><?php echo $param['buttontext']; ?></button>
             <span class="subscription-popup-success-message"></span>
         </div>
         <?php
         return ob_get_clean();
     }
-    
     add_shortcode('subscription-popup', 'subscriptionPopupFunc');
 
 ?>
